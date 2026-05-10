e7d5b4258ebbd6d231bd088a4df02787a22e5e6b
nua <enuesaa@gmail.com>
2026-05-10 17:19:49 +0900

    feat: polly Bidirectional Streaming API は東京リージョンでサポートされていないことがわかった

--- apps/leele-audiohandle/src/audiooutput.rs
+++ apps/leele-audiohandle/src/audiooutput.rs
@@ -12,18 +12,20 @@ use aws_sdk_polly::types::{
 };
 use aws_smithy_http::event_stream::EventStreamSender;
 use tokio::sync::mpsc;
-use futures_util::stream::StreamExt;
-// use aws_sdk_polly::types::StartSpeechSynthesisStreamResponseStream;
+use tokio_stream;
 
 const CHUNK_SIZE: usize = 1024;
 
 pub async fn handle_audio_output(msid: &str, text: &str) -> Result<()> {
-    let awsconf = aws_config::load_defaults(aws_config::BehaviorVersion::latest()).await
+    let awsconfvirginia = aws_config::load_defaults(aws_config::BehaviorVersion::latest()).await
         .to_builder()
         .region(Region::new("us-east-1"))
         .build();
-    let polly = aws_sdk_polly::Client::new(&awsconf);
-    // let iot = aws_sdk_iotdataplane::Client::new(&awsconf);
+    let awsconf = aws_config::load_defaults(aws_config::BehaviorVersion::latest()).await;
+    // for Bidirectional Streaming API 
+    // see https://docs.aws.amazon.com/polly/latest/dg/generative-voices.html
+    let polly = aws_sdk_polly::Client::new(&awsconfvirginia);
+    let iot = aws_sdk_iotdataplane::Client::new(&awsconf);
 
     let (tx, rx) = mpsc::channel::<Result<StartSpeechSynthesisStreamActionStream, StartSpeechSynthesisStreamActionStreamError>>(32);
 
@@ -64,11 +66,24 @@ pub async fn handle_audio_output(msid: &str, text: &str) -> Result<()> {
         match event {
             StartSpeechSynthesisStreamEventStream::AudioEvent(audio) => {
                 if let Some(chunk) = audio.audio_chunk {
-                    println!("音声チャンク: {} bytes", chunk.as_ref().len());
+                    println!("StartSpeechSynthesisStreamEventStream: AudioEvent {} bytes", chunk.as_ref().len());
+                    for sub_chunk in chunk.as_ref().chunks(CHUNK_SIZE) {
+                        println!("StartSpeechSynthesisStreamEventStream: AudioEvent SubChunk");
+                        let payload = json!({
+                            "session": msid,
+                            "data": base64::engine::general_purpose::STANDARD.encode(sub_chunk),
+                        });
+                        iot.publish()
+                            .topic("leele/d/m5cores3/audioout/chunk")
+                            .qos(1)
+                            .payload(payload.to_string().into_bytes().into())
+                            .send()
+                            .await?;
+                    }
                 }
             }
             StartSpeechSynthesisStreamEventStream::StreamClosedEvent(e) => {
-                println!("ストリーム完了: {:?}", e.request_characters);
+                println!("StartSpeechSynthesisStreamEventStream: StreamClosedEvent");
                 break;
             }
             _ => {}

c0aca130b7a5693d5260a7ec24b31100d7070562
nua <enuesaa@gmail.com>
2026-05-17 19:36:07 +0900

    refactor: agentcore runtime codes.

--- apps/leele-agentcore/app/main.py
+++ apps/leele-agentcore/app/main.py
@@ -7,53 +7,54 @@ from bedrock_agentcore.memory.integrations.strands.session_manager import AgentC
 from app.mcpclient import websearch
 import os
 
-MEMORY_ID = os.environ.get('MEMORY_ID', '')
+memory_id = os.environ.get('MEMORY_ID', '')
 
 app = BedrockAgentCoreApp()
+model = BedrockModel(
+    model_id='jp.anthropic.claude-haiku-4-5-20251001-v1:0',
+    max_tokens=300,
+    temperature=0.0,
+)
+sysprompt = [
+    SystemContentBlock(text='あなたはスマートスピーカーです。ちょうどいま人間と会話してます。概ね100文字程度で話をしてください。あなたの回答は人間へそのまま読み上げられます。そのため構造化せず文章で回答してください'),
+    SystemContentBlock(cachePoint={'type': 'default'}),
+]
 
 @app.entrypoint
-async def invoke(payload, context):
+async def invoke(payload):
     prompt = payload.get('prompt')
     si = payload.get('si', 'sinotspecified')
-    app.logger.info('prompt=%s, si=%s', prompt, si)
-    if prompt == "":
-        yield "Unknown Prompt"
+    if prompt == '':
+        yield 'Unknown Prompt'
         return
+    app.logger.info('prompt=%s, si=%s', prompt, si)
 
-    session_manager = AgentCoreMemorySessionManager(
-        agentcore_memory_config=AgentCoreMemoryConfig(memory_id=MEMORY_ID, session_id=si, actor_id='me')
+    sessionmanager = AgentCoreMemorySessionManager(
+        agentcore_memory_config=AgentCoreMemoryConfig(memory_id=memory_id, session_id=si, actor_id='me')
     )
-    with websearch as websearch_client:
-        tools = websearch_client.list_tools_sync()
-        agent = Agent(
-            model=BedrockModel(
-                model_id='jp.anthropic.claude-haiku-4-5-20251001-v1:0',
-                max_tokens=300,
-                temperature=0.0,
-            ),
-            system_prompt=[
-                SystemContentBlock(text='あなたはスマートスピーカーです。ちょうどいま人間と会話してます。概ね100文字程度で話をしてください。あなたの回答は人間へそのまま読み上げられます。そのため構造化せず文章で回答してください'),
-                SystemContentBlock(cachePoint={'type': 'default'}),
-            ],
-            tools=tools,
-            session_manager=session_manager,
-        )
-        stream = agent.stream_async(prompt)
-        buffer = ''
+    agent = Agent(
+        model=model,
+        system_prompt=sysprompt,
+        session_manager=sessionmanager,
+        tools=[websearch],
+    )
+    stream = agent.stream_async(prompt)
+    buffer = ''
 
-        async for event in stream:
-            if 'data' in event and isinstance(event['data'], str):
-                buffer += event['data']
-                while '。' in buffer:
-                    idx = buffer.index('。') + 1
-                    sentence = buffer[:idx].replace('\n', '').replace('#', '')
-                    buffer = buffer[idx:]
-                    app.logger.info('yield: %s', sentence)
-                    yield sentence
+    async for event in stream:
+        if 'data' in event and isinstance(event['data'], str):
+            buffer += event['data']
+            while '。' in buffer:
+                idx = buffer.index('。') + 1
+                sentence = buffer[:idx].replace('\n', '').replace('#', '')
+                buffer = buffer[idx:]
+                app.logger.info('yield: %s', sentence)
+                yield sentence
 
-        # last words
-        if buffer.strip():
-            yield buffer.strip()
+    lastWords = buffer.strip()
+    if len(lastWords) > 0:
+        app.logger.info('lastWords: %s', lastWords)
+        yield lastWords
 
 if __name__ == '__main__':
     app.run()
--- apps/leele-agentcore/app/mcpclient.py
+++ apps/leele-agentcore/app/mcpclient.py
@@ -1,11 +1,6 @@
 from mcp.client.streamable_http import streamable_http_client
 from strands.tools.mcp.mcp_client import MCPClient
-# from strands import tool
 
 websearch = MCPClient(
     lambda: streamable_http_client('https://mcp.exa.ai/mcp'),
 )
-
-# @tool
-# def add_numbers(a: int, b: int) -> int:
-#     return a+b
